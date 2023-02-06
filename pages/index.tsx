3b1003fee35d071f4ce0fb8a759e6418e4b08fd3
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-06 22:54:05 +0100

    Commit: Created Blog post card and fetched post from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import Header from '../Components/Header'
 import Banner from '../Components/Banner'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings'
+import Link from 'next/link'
 
 interface Props {
   posts: [Post]
 }
 
 const Home = ({ posts }: Props) => {
-  console.log(posts);
   return (
     <div className="font-serif max-w-7xl mx-auto">
       <Head>
@@ -21,6 +21,23 @@ const Home = ({ posts }: Props) => {
       <Header />
 
       <Banner />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6 my-5">
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='group cursor-pointer border rounded-xl shadow overflow-hidden'>
+              <img src={urlFor(post.mainImage).url()!} alt={post.title} className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'/>
+              <div className="flex justify-between p-5 bg-white">
+                <div className="">
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p>{post.description} by {post.author.name}</p>
+                </div>
+                <img src={urlFor(post.author.image).url()!} alt={post.author.name} className='h-12 w-12 rounded-full'/>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
 
     </div>
   )
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,27 @@
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
+
+const Post = () => {
+    return (
+        <div>Post</div>
+    )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == 'post']{
+  _id,
+  slug {
+    current
+  }
+}`
+
+const posts = await sanityClient.fetch(query)
+
+const paths = posts.map((post: Post) => ({
+    params : {
+        slug: post.slug.current
+    }
+}))
+}
