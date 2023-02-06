c7313f4b3940d658c14eb8c0f3016f6a3d395f7e
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-06 14:11:01 +0100

    Commit: connecting to sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../Components/Header'
 import Banner from '../Components/Banner'
+import {sanityClient, urlFor} from '../sanity'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+const Home: NextPage = (props: Props) => {
   return (
     <div className="font-serif max-w-7xl mx-auto">
       <Head>
@@ -15,8 +19,31 @@ const Home: NextPage = () => {
       <Header />
 
       <Banner />
+      
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async() => {
+  const query = `*[_type == 'post']{
+  _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    slug,
+    description, 
+    mainImage
+}`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
