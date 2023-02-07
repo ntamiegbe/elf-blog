45d47fe32f5e090fe65ec306c374a8c56b186ccd
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-07 09:49:05 +0100

    Created slug file to find and populate the pages that exists

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,12 @@
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
+import { GetStaticProps } from 'next'
 
-const Post = () => {
+interface Props {
+    post: Post
+}
+
+const Post = ({ post }: Props) => {
     return (
         <div>Post</div>
     )
@@ -10,18 +15,57 @@ const Post = () => {
 export default Post
 
 export const getStaticPaths = async () => {
+    // Find the paths or pages that exists
     const query = `*[_type == 'post']{
-  _id,
-  slug {
-    current
-  }
-}`
+        _id,
+        slug {
+            current
+        }
+    }`
 
-const posts = await sanityClient.fetch(query)
+    const posts = await sanityClient.fetch(query)
 
-const paths = posts.map((post: Post) => ({
-    params : {
-        slug: post.slug.current
+    const paths = posts.map((post: Post) => ({
+        params: {
+            slug: post.slug.current
+        }
+    }))
+
+    return {
+        paths,
+        fallback: 'blocking'
     }
-}))
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    // Populate the information for the page
+    const query = `*[_type == 'post' && slug.current == 'first-blog-post'][0]{
+        _id,
+        _createdAt,
+        title,
+        author -> {
+            name,
+            image
+        },
+        slug,
+        description,
+        mainImage,
+        body
+    }`
+
+    const post = await sanityClient.fetch(query, {
+        slug: params?.slug
+    })
+
+    if (!post) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            post,
+        }
+    }
 }
