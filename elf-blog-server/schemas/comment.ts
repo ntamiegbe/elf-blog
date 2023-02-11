993469c968d2c775394f586ee7e3c5ea7dab5f61
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-11 23:09:23 +0100

    Commit: Creating comment schema

--- elf-blog-server/schemas/comment.ts
+++ elf-blog-server/schemas/comment.ts
@@ -1,14 +1,31 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity'
 
-export default {
+export default defineType({
     name: 'comment',
     type: 'document',
-  fields: [
-    defineField({
-      name: 'name',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description
-}
+    fields: [
+        defineField({
+            name: 'name',
+            type: 'string',
+        }),
+        defineField({
+            name: 'approved',
+            type: 'boolean',
+            title: 'Approved',
+            description: 'Comments will not show on the site without approval'
+        }),
+        defineField({
+            name: 'email',
+            type: 'string',
+        }),
+        defineField({
+            name: 'comment',
+            type: 'text',
+        }),
+        defineField({
+            name: 'post',
+            type: 'array',
+            of: [{ type: 'reference', to: { type: 'post' } }],
+        }),
+    ]
+})
