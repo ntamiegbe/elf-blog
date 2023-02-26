02d31f170fc8556e739f43dfff1a82fb99e1361d
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-26 12:01:15 +0100

    Commit: changes

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -1,12 +1,13 @@
 const Banner = () => {
-  return (
-      <div className="bg-green-100 py-20 flex flex-col items-center w-full justify-center h-[450px] border-t border-b border-green-400">
-          <div className="space-y-5 px-10">
-              <h4 className='text-[14px] text-green-600 text-center'>Unlocking Potential, Changing Lives</h4>
-              <h2 className='text-6xl text-center'> Journal of Humanities</h2>
-              <p className='text-[19px] text-center'>Discover our mission, impact, and stories as we strive to promote literacy and education for all.</p>
-          </div>
-      </div>  )
-}
+    return (
+        <div className="bg-green-100 py-20 flex flex-col items-center w-full justify-center h-[450px] border-t border-b border-green-400">
+            <div className="space-y-5 px-10">
+                <h4 className="text-[14px] text-green-600 text-center">Unlocking Potential, Changing Lives</h4>
+                <h2 className="text-6xl text-center">Journal of Humanities</h2>
+                <p className="text-[19px] text-center">Discover our mission, impact, and stories as we strive to promote literacy and education for all.</p>
+            </div>
+        </div>
+    );
+};
 
-export default Banner
+export default Banner;
