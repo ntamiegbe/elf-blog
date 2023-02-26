23543e67fdd1c54c7fec0de93f0a0e24e3f1d13d
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-26 06:10:27 +0100

    Commit: Changes to banner

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -2,7 +2,7 @@ const Banner = () => {
   return (
       <div className="bg-green-100 py-10 flex flex-col items-center w-full justify-center h-[450px] border-t border-b border-green-400">
           <div className="space-y-5 px-10">
-              <h4 className='text-[14px] text-center'>Unlocking Potential, Changing Lives</h4>
+              <h4 className='text-[14px] text-green-600 text-center'>Unlocking Potential, Changing Lives</h4>
               <h2 className='text-6xl text-center'> Journal of Humanities</h2>
               <p className='text-[19px] text-center'>Discover our mission, impact, and stories as we strive to promote literacy and education for all.</p>
           </div>
